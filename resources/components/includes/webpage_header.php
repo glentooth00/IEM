5d5a617c465b1bd4363e10219efeed28b9d852db
Glen <glendonnalpasan1@gmail.com>
2026-02-06 16:49:12 +0800

    nav bar component creation

---
+++ resources/components/includes/webpage_header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
+
+    <link rel="stylesheet" type="text/css" href="public/assets/css/web_page_navbar.css">
+    <title><?= $pagetitle ?></title>
+</head>
+
+<body>
---
+++ resources/components/sections/top_menu.php
@@ -0,0 +1,34 @@
+<nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+        <!-- Logo -->
+        <div class="" href="#">
+            <img src="public/assets/images/fakelogo.png" id="fakeLogo"
+                style="height: 80px;position:relative;margin-left: 20px;">
+        </div>
+
+        <!-- University Name -->
+        <div class="navbar-item" href="#" style="font-weight: bold; font-size: 1.2em;color:white;">
+            University Name Here
+        </div>
+
+        <!-- Burger menu for mobile -->
+        <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarMenu">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+        </a>
+    </div>
+
+    <div id="navbarMenu" class="navbar-menu">
+        <div class="navbar-end">
+            <a class="navbar-item" href="#">Home</a>
+            <a class="navbar-item" href="#">About</a>
+            <a class="navbar-item" href="#">Administration</a>
+            <a class="navbar-item" href="#">Academics</a>
+            <a class="navbar-item" href="#">Research and Extension</a>
+            <a class="navbar-item" href="#">External Campuses</a>
+            <a class="navbar-item" href="#">International Collaboration</a>
+            <a class="navbar-item" href="#">Legal Office Affairs</a>
+        </div>
+    </div>
+</nav>
